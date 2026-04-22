bc8a5722e1a6fe6c2c1b078802d3e760d3c1593f
Luca Atella <atellaluca@outlook.it>
2026-04-22 17:01:51 +0200

    feat: add dynamodb logics

--- src/lambda-car-backend/infrastructure/dynamodb/client.py
+++ src/lambda-car-backend/infrastructure/dynamodb/client.py
@@ -4,4 +4,13 @@ from infrastructure.dynamodb.config import load_dynamodb_config
 
 def create_dynamodb_resource():
     config = load_dynamodb_config()
-    return boto3.resource("dynamodb", region_name=config.region_name)
+
+    kwargs = {
+        "service_name": "dynamodb",
+        "region_name": config.region_name,
+    }
+
+    if config.endpoint_url:
+        kwargs["endpoint_url"] = config.endpoint_url
+
+    return boto3.resource(**kwargs)
--- src/lambda-car-backend/infrastructure/dynamodb/mappers/trip_mapper.py
+++ src/lambda-car-backend/infrastructure/dynamodb/mappers/trip_mapper.py
@@ -1,5 +1,6 @@
 from domain.trip import Trip
 from uuid import UUID
+from datetime import datetime
 
 def trip_to_dynamodb_item(trip: Trip) -> dict:
     return {
@@ -7,10 +8,10 @@ def trip_to_dynamodb_item(trip: Trip) -> dict:
         'car_id': str(trip.car_id),
         'user_id': str(trip.user_id),
         'start_position': trip.start_position,
-        'start_date': trip.start_date,
+        'start_date': trip.start_date.isoformat(),
         'start_km': trip.start_km,
         'end_position': trip.end_position,
-        'end_date': trip.end_date,
+        'end_date': trip.end_date.isoformat() if trip.end_date else None,
         'end_km': trip.end_km
     }
 
@@ -20,10 +21,10 @@ def dynamodb_item_to_trip(item: dict) -> Trip:
         car_id=UUID(item['car_id']),
         user_id=UUID(item['user_id']),
         start_position=item['start_position'],
-        start_date=item['start_date'],
+        start_date=datetime.fromisoformat(item['start_date']),
         start_km=item['start_km'],
         end_position=item.get('end_position'),
-        end_date=item.get('end_date'),
+        end_date=datetime.fromisoformat(item['end_date']) if item.get('end_date') else None,
         end_km=item.get('end_km')
     )
 
